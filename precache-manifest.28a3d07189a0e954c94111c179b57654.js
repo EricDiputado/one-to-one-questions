01a35bff69d998b11d1450a5e2055eda454cf40d
Eric Diputado <ericdiputado@gmail.com>
2019-07-24 23:50:17 +1200

    Updates

---
+++ precache-manifest.28a3d07189a0e954c94111c179b57654.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae1be9da54abf7d4646630868af5c970",
+    "url": "/one-to-one-questions/index.html"
+  },
+  {
+    "revision": "5508e44de96299441576",
+    "url": "/one-to-one-questions/static/css/2.741d7841.chunk.css"
+  },
+  {
+    "revision": "8c017230133b42133c90",
+    "url": "/one-to-one-questions/static/css/main.907ffa02.chunk.css"
+  },
+  {
+    "revision": "5508e44de96299441576",
+    "url": "/one-to-one-questions/static/js/2.e37b9cf7.chunk.js"
+  },
+  {
+    "revision": "8c017230133b42133c90",
+    "url": "/one-to-one-questions/static/js/main.8b941a11.chunk.js"
+  },
+  {
+    "revision": "4367431c5fe3f7b6e2e7",
+    "url": "/one-to-one-questions/static/js/runtime~main.b5b48bce.js"
+  },
+  {
+    "revision": "76f970b5cb87b4eb9f53f30792995811",
+    "url": "/one-to-one-questions/static/media/Logo.76f970b5.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1b018ed1ee984b55fec1b223625edc8e.js"
+  "/one-to-one-questions/precache-manifest.28a3d07189a0e954c94111c179b57654.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/one-to-one-questions/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
