d5afd1acf3b254f64e50e5eac5b0a8300e6c4ae9
Eric Diputado <ericdiputado@gmail.com>
2019-07-24 23:18:27 +1200

    Updates

---
+++ precache-manifest.f694334ce69fd310bff73f2d8e93b55a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22085a4cc3b382cb8b039dedbf994190",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5508e44de96299441576",
+    "url": "/static/css/2.741d7841.chunk.css"
+  },
+  {
+    "revision": "76acce04d1245821b51f",
+    "url": "/static/css/main.907ffa02.chunk.css"
+  },
+  {
+    "revision": "5508e44de96299441576",
+    "url": "/static/js/2.e37b9cf7.chunk.js"
+  },
+  {
+    "revision": "76acce04d1245821b51f",
+    "url": "/static/js/main.e7e94aeb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "76f970b5cb87b4eb9f53f30792995811",
+    "url": "/static/media/Logo.76f970b5.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.054774adbe886ee6e3c29227ef1745b5.js"
+  "/precache-manifest.f694334ce69fd310bff73f2d8e93b55a.js"
 );
 
 self.addEventListener('message', (event) => {
